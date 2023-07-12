d17cf520e763198378bea5c1594e72d08721ca6a
Zvi Grinberg <zgrinber@redhat.com>
2023-07-12 17:12:11 +0300

    add documentation for script
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

---
+++ podman-image-layers-total-size/README.sh
@@ -0,0 +1,18 @@
+# Usage
+
+
+- Depth: Depth of tree file system in details of files and sizes that will be shown in each image' layer of report
+
+
+
+```shell
+./show-layers.sh image:tag Depth
+```
+
+
+
+For example:
+
+```shell
+./show-layers.sh docker.io/library/mongo:4.4 2
+```
